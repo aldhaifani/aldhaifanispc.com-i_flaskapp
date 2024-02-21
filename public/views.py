e636c6245e15db5c48ef5400b87e2bda99e43aee
Tareq <tareqaldhaifani@gmail.com>
2024-02-21 17:46:54 +0400

    dual language base routes setup

--- public/views.py
+++ public/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
@@ -6,5 +6,21 @@ views = Blueprint("views", __name__)
 @views.route("/")
 @views.route("/home")
 @views.route("/home/")
-def redirect_lang():
-    return "Hello World!"
+def landing_page():
+    return render_template("landing_page.html")
+
+
+@views.route("/en")
+@views.route("/en/")
+@views.route("/en/home")
+@views.route("/en/home/")
+def home_en():
+    return "english"
+
+
+@views.route("/ar")
+@views.route("/ar/")
+@views.route("/ar/home")
+@views.route("/ar/home/")
+def home_ar():
+    return "arabic"
